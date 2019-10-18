72cec8f28f5ee14c4d9a21589a7ff3729836f978
Marcos Luna <marcos@starluna.net>
2019-10-17 20:47:38 -0400

    Script to download block group data for all NE states using tidycensus with geometry

---
+++ Demographics.R
@@ -0,0 +1,31 @@
+# TCI Analysis of Demographic Data
+
+library(tidycensus)
+library(tidyverse)
+library(tmap)
+options(tigris_use_cache = TRUE)
+
+census_api_key("f2776fbc29cf847505de9308a82c8d65290d16b3")
+
+# Load list of variables to identify tables of interest
+v17 <- load_variables(2017, "acs5", cache = TRUE)
+
+# tidycensus df with stat codes
+# unique(fips_codes$state)
+
+# create vector of states to download
+ne_states <- c("CT","MA","ME","NH","RI","VT")
+
+# use purrr::reduce function in combination with sf::rbind to acquire desired states from tidycensus. 
+ne_pop_sf <- reduce(
+  map(ne_states, function(x) {
+    get_acs(geography = "block group", 
+            variables = c(totalpop = "B03002_001", 
+                          whitepop = "B03002_003"),
+            state = x, output = "wide", geometry = TRUE)
+    }),
+  rbind
+)
+
+# map it out
+tm_shape(ne_pop_sf) + tm_polygons(col = "totalpopE")
