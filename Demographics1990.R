e103409870a56c2bb29def6bca76ea41334782b2
Marcos Luna <marcos@starluna.net>
2020-01-20 15:30:46 -0500

    Removed code using tidycensus and tigris for getting 1990 census block group data because GEOIDs do not match up. Added code to use IPUMs data

--- Demographics1990.R
+++ Demographics1990.R
@@ -1,150 +1,66 @@
 # Demographics from 1990 decennial census
 
-library(tidycensus)
 library(tidyverse)
 library(tmap)
 library(sf)
-library(tigris)
-options(tigris_use_cache = TRUE, tigris_class = "sf")
-library(raster)
 
-census_api_key("f2776fbc29cf847505de9308a82c8d65290d16b3")
+# Download data from NHGIS.
+# Documentation for NHGIS datasets is available at https://www.nhgis.org/documentation.
+# Research using NHGIS data should cite it as:
+#   Steven Manson, Jonathan Schroeder, David Van Riper, and Steven Ruggles. IPUMS National Historical Geographic Information System: Version 14.0 [Database]. Minneapolis, MN: IPUMS. 2019. http://doi.org/10.18128/D050.V14.0
+# For policy briefs or articles in the popular press, we recommend that you cite the use of NHGIS data as follows:
+#   IPUMS NHGIS, University of Minnesota, www.nhgis.org
 
-# Load list of variables to identify tables of interest
-v90 <- load_variables(1990, "sf3", cache = TRUE)
+# read in tabular data
+ne_blkgrp_stf1 <- read_csv("DATA/nhgis0001_csv/nhgis0001_ds120_1990_blck_grp.csv") %>% 
+  transmute(GISJOIN = GISJOIN,
+            YEAR = YEAR,
+            COUNTY = COUNTY,
+            STATE = STATE,
+            totalpop = ET1001,
+            households = EUO001,
+            nhwhite = ET2001,
+            nhblack = ET2002,
+            nhamerind = ET2003,
+            nhasian = ET2004,
+            nhother = ET2005,
+            hispanic = ET2006 + ET2007 + ET2008 + ET2009 + ET2010,
+            minority = nhblack + nhamerind + nhasian + nhother + hispanic,
+            under5 = ET3001 + ET3002 + ET3003,
+            over64 = ET3027 + ET3028 + ET3029 + ET3030 + ET3031)
 
-### CENSUS POLYGONS
+ne_blkgrp_stf3 <- read_csv("DATA/nhgis0001_csv/nhgis0003_ds123_1990_blck_grp.csv") %>% 
+  transmute(GISJOIN = GISJOIN,
+            langIsolHH = E27002 + E27004 + E27006,
+            noHSdip = E33001 + E33002,
+            lowInc = E1C001 + E1C002 + E1C003 + E1C004 + E1C005 + 
+              E1C006 + E1C007 + E1C008)
 
-# create vector of New England states
-ne_states <- c("CT","MA","ME","NH","RI","VT")
+# read in 1990 block group shapefiles and rbind into one sf
+ne_blkgrp_sf90 <- list.files(path = "DATA/shapefiles/nhgis0001_shape",
+                             pattern = "*.shp$") %>% 
+  tools::file_path_sans_ext() %>% 
+  lapply(., function(x){
+           st_read(dsn = "DATA/shapefiles/nhgis0001_shape",
+                   layer = x) %>% 
+             mutate(GISJOIN = as.character(GISJOIN),
+                    GISJOIN2 = as.character(GISJOIN2))
+         }) %>%
+  do.call(rbind, .) %>% 
+  lwgeom::st_make_valid()
 
-# use purrr::reduce function in combination with sf::rbind to download block groups for list of states and then bind them to one sf. 
-ne_blkgrp_90 <- reduce(
-  map(ne_states, function(x) {
-    get_decennial(geography = "block group",
-                  sumfile = "sf3",
-                  year = 1990,
-            variables = c(totalpop = "P0010001", 
-                          households = "P0050001",
-                          hispanic = "P0100001",
-                          nhwhite = "P0120001",
-                          nhblack = "P0120002",
-                          nhamerind = "P0120003",
-                          nhasian = "P0120004",
-                          nhother = "P0120005",
-                          age_u1 = "P0130001",
-                          age1_2 = "P0130002",
-                          age3_4 = "P0130003",
-                          age65_69 = "P0130027",
-                          age70_74 = "P0130028",
-                          age75_79 = "P0130029",
-                          age80_84 = "P0130030",
-                          age85 = "P0130031",
-                          spanLangIsolHH = "P0290002",
-                          asianLangIsolHH = "P0290004",
-                          othrLangIsolHH = "P0290006",
-                          edLt9th = "P0570001",
-                          ed9_12 = "P0570002",
-                          incTopovUndr.5 = "P1210001",
-                          incTopov.5_.74 = "P1210002",
-                          incTopov.75_.99 = "P1210003",
-                          incTopov1_1.24 = "P1210004",
-                          incTopov1.25_1.49 = "P1210005",
-                          incTopov1.5_1.74 = "P1210006",
-                          incTopov1.75_1.84 = "P1210007",
-                          incTopov1.85_1.99 = "P1210008"),
-            state = x, output = "wide")
-  }),
-  rbind
-)
+# Join demographics to geometry
+ne_blkgrp_sf90 <- ne_blkgrp_sf90 %>% 
+  left_join(., ne_blkgrp_stf1, by = "GISJOIN") %>% 
+  left_join(., ne_blkgrp_stf3, by = "GISJOIN")
 
-
-# use purrr::reduce function in combination with sf::rbind to download states for list of states and then bind them to one sf. 
-ne_state_90 <- reduce(
-  map(ne_states, function(x) {
-    get_decennial(geography = "state",
-                  sumfile = "sf3",
-                  year = 1990,
-                  variables = c(totalpop = "P0010001", 
-                                households = "P0050001",
-                                hispanic = "P0100001",
-                                nhwhite = "P0120001",
-                                nhblack = "P0120002",
-                                nhamerind = "P0120003",
-                                nhasian = "P0120004",
-                                nhother = "P0120005",
-                                age_u1 = "P0130001",
-                                age1_2 = "P0130002",
-                                age3_4 = "P0130003",
-                                age65_69 = "P0130027",
-                                age70_74 = "P0130028",
-                                age75_79 = "P0130029",
-                                age80_84 = "P0130030",
-                                age85 = "P0130031",
-                                spanLangIsolHH = "P0290002",
-                                asianLangIsolHH = "P0290004",
-                                othrLangIsolHH = "P0290006",
-                                edLt9th = "P0570001",
-                                ed9_12 = "P0570002",
-                                incTopovUndr.5 = "P1210001",
-                                incTopov.5_.74 = "P1210002",
-                                incTopov.75_.99 = "P1210003",
-                                incTopov1_1.24 = "P1210004",
-                                incTopov1.25_1.49 = "P1210005",
-                                incTopov1.5_1.74 = "P1210006",
-                                incTopov1.75_1.84 = "P1210007",
-                                incTopov1.85_1.99 = "P1210008"),
-                  state = x, output = "wide")
-  }),
-  rbind
-)
+# check for missing data
+sum(is.na(ne_blkgrp_sf90$totalpop))
+tmap_mode("view")
+tm_shape(ne_blkgrp_sf90) + tm_fill("totalpop", alpha = 0.3) + tm_borders()
 
 
-# Compute estimates for populations of interest
-ne_blkgrp_90 <- ne_blkgrp_90 %>% 
-  mutate(minority = hispanic + 
-           nhwhite + nhblack + nhasian + nhamerind + nhother,
-         under5 = age_u1 + age1_2 + age3_4,
-         over64 = age65_69 + age70_74 + age75_79 + age80_84 + age85,
-         langisol = spanLangIsolHH + asianLangIsolHH + othrLangIsolHH,
-         noHSdip = edLt9th  + ed9_12,
-         lowInc = incTopovUndr.5 + incTopov.5_.74 + incTopov.75_.99 +
-           incTopov1_1.24 + incTopov1.25_1.49 + incTopov1.25_1.49 +
-           incTopov1.5_1.74 + incTopov1.75_1.84 + incTopov1.85_1.99)
 
-# acquire the polygons for 1990 block groups using tigris::rbind_tigris
-ne_blkgrps_sf90 <- rbind_tigris(
-  lapply(
-    ne_states, function(x){
-      block_groups(state = x, cb = TRUE, year = 1990)
-    }
-  )
-)
-
-# join df to sf
-ne_blkgrps_sf90b <- ne_blkgrps_sf90 %>% 
-  dplyr::select(-NAME) %>%
-  left_join(., ne_blkgrp_90, by = c("GEOID" = "GEOID"))
-
-sum(is.na(ne_blkgrps_sf90b$totalpop))
-rm(ne_blkgrps_sf90b)
-
-# Add in state names and abbreviations based on STATEFP
-ne_blkgrps_sf90 <- ne_blkgrps_sf90 %>% 
-  mutate(STATE = case_when(
-    STATEFP == "09" ~ "Connecticut",
-    STATEFP == "23" ~ "Maine",
-    STATEFP == "25" ~ "Massachusetts",
-    STATEFP == "33" ~ "New Hampshire",
-    STATEFP == "44" ~ "Rhode Island",
-    STATEFP == "50" ~ "Vermont"),
-  STABBR = case_when(
-    STATEFP == "09" ~ "CT",
-    STATEFP == "23" ~ "ME",
-    STATEFP == "25" ~ "MA",
-    STATEFP == "33" ~ "NH",
-    STATEFP == "44" ~ "RI",
-    STATEFP == "50" ~ "VT"))
 
 # Compute CO2 by block group
 # Read in DARTE Annual On-road CO2 Emissions on a 1-km Grid. See https://daac.ornl.gov/cgi-bin/dsviewer.pl?ds_id=1735 
