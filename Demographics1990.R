ffa51a4864942aa856185e38a32bdce2f8c315b1
Marcos Luna <marcos@starluna.net>
2020-01-21 17:30:28 -0500

    Added code to use areal interpolation for 1990 CO2 values from DARTE census block groups to 1990 census block groups for weighted population values

--- Demographics1990.R
+++ Demographics1990.R
@@ -3,6 +3,7 @@
 library(tidyverse)
 library(tmap)
 library(sf)
+library(areal) # for areal weighted interpolation
 
 # Download data from NHGIS.
 # Documentation for NHGIS datasets is available at https://www.nhgis.org/documentation.
@@ -14,7 +15,6 @@ library(sf)
 # read in tabular data
 ne_blkgrp_stf1 <- read_csv("DATA/nhgis0001_csv/nhgis0001_ds120_1990_blck_grp.csv") %>% 
   transmute(GISJOIN = GISJOIN,
-            GEOID = paste0(STATEA,COUNTYA,TRACTA,BLCK_GRPA),
             YEAR = YEAR,
             COUNTY = COUNTY,
             STATE = STATE,
@@ -37,7 +37,7 @@ ne_blkgrp_stf3 <- read_csv("DATA/nhgis0001_csv/nhgis0003_ds123_1990_blck_grp.csv
             lowInc = E1C001 + E1C002 + E1C003 + E1C004 + E1C005 + 
               E1C006 + E1C007 + E1C008)
 
-# read in 1990 block group shapefiles and rbind into one sf
+# read in 1990 block group shapefiles for NE states and rbind into one sf
 ne_blkgrp_sf90 <- list.files(path = "DATA/shapefiles/nhgis0001_shape",
                              pattern = "*.shp$") %>% 
   tools::file_path_sans_ext() %>% 
@@ -57,33 +57,45 @@ ne_blkgrp_sf90 <- ne_blkgrp_sf90 %>%
 
 # check for missing data
 sum(is.na(ne_blkgrp_sf90$totalpop))
-tmap_mode("view")
-tm_shape(ne_blkgrp_sf90) + tm_fill("totalpop", alpha = 0.3) + tm_borders()
+# tmap_mode("view")
+# tm_shape(ne_blkgrp_sf90) + tm_fill("totalpop", alpha = 0.3) + tm_borders()
 
 
-# Import CO2 by block group from DARTE geodatabase. See https://daac.ornl.gov/cgi-bin/dsviewer.pl?ds_id=1735 
-onroadCO2 <- st_read(dsn = "DATA/DARTE/DARTE_v2.gdb/DARTE_v2.gdb", 
-                     layer = "DARTE_v2_blockgroup_kgco2_1980_2017") %>% 
-  mutate(GEOID = as.character(GEOID))
-
-# Spatially join CO2 data to blockgroups with demographics because GEOIDs don't match. Should reapportion CO2 based on proportion of overlap. 
-ne_blkgrp_sf90 <- onroadCO2 %>% 
-  select(kgco2_1990) %>% 
-  st_transform(., st_crs(ne_blkgrp_sf90)) %>% 
-  st_join(ne_blkgrp_sf90, ., largest = TRUE)
+# Use areal weighted interpolation to assign CO2 values to 1990 block groups from 2017 block groups on which onroadCO2 is based.  
+# First, transform to projected CRS; Albers Equal Area
+ne_blkgrp_sf_aea <- ne_blkgrp_sf %>% 
+  select(GEOID,kgco2_1990) %>% 
+  st_transform(., crs = 2163)
+ne_blkgrp_sf90_aea <- st_transform(ne_blkgrp_sf90, crs = 2163)
+# Execute areal weighted interpolation
+ne_blkgrp_df90_CO2 <- ne_blkgrp_sf90_aea %>% 
+  select(GISJOIN) %>% 
+  aw_interpolate(.,
+                 tid = GISJOIN,
+                 source = ne_blkgrp_sf_aea,
+                 sid = GEOID,
+                 weight = "sum",
+                 output = "tibble",
+                 extensive = "kgco2_1990")
 
 # check for NAs
-sum(is.na(ne_blkgrp_sf90$kgco2_1990))
+sum(is.na(ne_blkgrp_df90_CO2$kgco2_1990))
+
+# join CO2 values to 1990 block groups
+ne_blkgrp_sf90 <- left_join(ne_blkgrp_sf90, ne_blkgrp_df90_CO2, by="GISJOIN")
 
 # clean up
-rm(list = ls(pattern = "stf|onroad"))
+rm(list = ls(pattern = "stf|aea|df90"))
 
 # load("DATA/ne_layers.rds")
 
 # save with other files
 save(ne_blkgrp_sf,
+     ne_blkgrp_sf90,
      ne_tracts_sf,
      ne_towns_sf, 
+     ne_towns_sf_pts,
+     ne_states_sf_cb,
      ne_states,
      ne_blkgrp_sf90,
      file = "DATA/ne_layers.rds")
